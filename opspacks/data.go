04c46630f85327c2cd659304151dfa16d40cafb1
Webb249 <tom.webb249@gmail.com>
2017-02-17 14:32:03 +0000

    Update data.go

--- opspacks/data.go
+++ opspacks/data.go
@@ -1,6 +1,58 @@
 package opsviewplugin
 
-import "fmt"
+import(
+	"fmt"
+	"net/http"
+	"io/ioutil"
+	"strconv"
+	"errors"
+	"encoding/json"
+	)
+
+// Used to fetch metrics from a connection
+// Give it the address without the http://
+func Fetch(HostAddress string) string {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", "https://"+HostAddress, nil)
+	ExitAtError(err, "Cannot create connection request to "+HostAddress)
+
+	resp, err := client.Do(req)
+	ExitAtError(err, "Could Not Connect to "+HostAddress)
+
+	if resp.StatusCode != 200 {
+		Exit(CRITICAL, "Could Not Connect to "+HostAddress+" Response Code: "+string(resp.Status))
+	}
+
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	ExitAtError(err, "Error reading output from "+HostAddress)
+
+	return string(bodyText)
+}
+
+// Used to fetch metrics from a connection that requires authentication
+// Give it the address without the http://
+func FetchWithAuth(HostAddress string, Username string, Password string) string {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", HostAddress, nil)
+	ExitAtError(err, "Cannot create connection request to "+HostAddress)
+
+	if (Username != "") && (Password != "") {
+		req.SetBasicAuth(Username, Password)
+	}
+	resp, err := client.Do(req)
+	ExitAtError(err, "Could Not Connect to "+HostAddress)
+
+	if resp.StatusCode != 200 {
+		Exit(CRITICAL, "Could Not Connect to "+HostAddress+" Response Code: "+string(resp.Status))
+	}
+
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	ExitAtError(err, "Error reading output from "+HostAddress)
+
+	return string(bodyText)
+}
 
 // Accepts a warning and critical level to test against a value given to be evaluated.
 // Tests if the value is higher than the warning and critical levels unless reverse is equal to true
@@ -25,11 +77,18 @@ func Evaluate(warning int, critical int, value int, reverse bool) Status {
 
 func GetPercentOf(used int, total int)int{
   floatValue := (float64(used) / float64(total)) * float64(100)
+	if (floatValue < 0) || (floatValue > 100) {
+		Exit(CRITICAL, "Error converting values to percentage")
+	}
   return int(floatValue)
 }
 
 func GetPercentOfF(used float64, total float64)float64{
-  return (used / total) * 100
+	floatValue := (used / total) * 100
+	if (floatValue < 0) || (floatValue > 100) {
+		Exit(CRITICAL, "Error converting values to percentage")
+	}
+  return floatValue
 }
 
 
@@ -46,7 +105,7 @@ func ExitAtErrorPrint(err error, message string) {
 	}
 }
 
-func removeDecimal(value float64) float64 {
+func RemoveDecimal(value float64) float64 {
 	intValue := int(value)
 	return float64(intValue)
 }
@@ -85,4 +144,39 @@ func ConvertToGigaBytes(value int) int {
 func ConvertToGigaBytesf(value float64) float64 {
   returnValue :=  ((value / 1024) / 1024) / 1024
   return returnValue
+}
+
+// Checks if the port is empty and if it is a value that can be the port number
+func CheckPort(portValue string) {
+	port, err := strconv.Atoi(portValue)
+	ExitAtError(err, "Error converting port to integer")
+	if (portValue != "") && ((port < 1) || (port > 65535)) {
+		err = errors.New("Port check error")
+		ExitAtError(err, "Port must be between 1 and 65535")
+	}
+}
+
+// Checks the flag has a value
+// Arguments are in the order flagValue, flagName, flagCharater repeated
+func CheckFlags(args ...string) {
+	for i := range args {
+		if args[i] == "" {
+			err := errors.New("Flag check error")
+			ExitAtError(err, args[i+1]+" (-"+args[i+2]+") is a required argument")
+		}
+		i =+ 2
+	}
+}
+
+func DecodeJsonToMap(bodyText string) map[string]interface{} {
+	var m interface{}
+
+	json.Unmarshal([]byte(bodyText), &m)
+	if mapValue, ok := m.(map[string]interface{}); ok {
+		return mapValue
+	} else {
+			Exit(CRITICAL, "Error Converting Body Text to string")
+	}
+
+	return nil
 }
